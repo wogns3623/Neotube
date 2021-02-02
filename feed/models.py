34b34ccaf65b3f865301417acb5507dfd674a3df
Junsu <sunsuking@gmail.com>
2021-02-02 23:10:33 +0900

    Add Model Feed: link between neotuber and subscriber

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import Account
+
 
 # Create your models here.
+class Feed(models.Model):
+    neotuber = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'너튜버: {self.neotuber}, 구독자: {self.subscriber}'
