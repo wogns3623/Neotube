da15ff557177a38488fb67c95b44cc7d1f913d40
MinJunsu <sunsuking@gmail.com>
2021-02-17 01:45:04 +0900

    Reset Neotube Models

--- backend/Neotube/Neotube/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-
-def main_page(request):
-    return redirect('http://www.neotube.kro.kr')             # view_name 사용
--- backend/Neotube/watch/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Video, VideoLike, Category, CommentLike, CommentDisLike, VideoDisLike, Comment, Tag
-
-
-# Create your views here.
-
