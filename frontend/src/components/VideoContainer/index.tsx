cd4afaf547a37eaec8ca5b356fec91a5dc52893b
Jaehun Kim <wogns3623@gmail.com>
2021-02-24 20:06:22 +0900

    Revert "Modify DescIcon.scss & Extract LoginButton component"
    This reverts commit bad6cade47bdc98f86cb2e1ee85715fc95ac3025.

--- frontend/src/components/VideoContainer/index.tsx
+++ frontend/src/components/VideoContainer/index.tsx
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import "VideoContainer.scss";
-import imageTest from "D:/Neotube/frontend/src/styles/images/ilbuni.png";
-import CreateAt from "./CreateAt";
-// require() 안됨.
-// 상대경로 안됨
+
+import "./VideoContainer.scss";
 
 export type VideoData = {
   uploader: number;
@@ -21,15 +17,6 @@ type VideoBoxProps = {
   videoData: VideoData;
 };
 
-// todo list
-// position공부해서 화면 조정하기 (0)
-// react 이벤트 핸들링 하기
-// - videoview 띄우기
-// - 영상 이미지 focus : 영상 출력(0.5초 뒤) + runtime:none(0.5초 뒤) hidden: ''(바로)
-// - hidden focus: overflow만들기
-// videoName 두줄 + ... 만들기
-// hiddenadd focus: block, onclik: overflow뜨기
-
 const VideoBox = ({ videoData }: VideoBoxProps) => {
   const {
     uploader,
@@ -40,63 +27,26 @@ const VideoBox = ({ videoData }: VideoBoxProps) => {
     watch_count,
     created_at,
   } = videoData;
-
-  const ThunmbnailImg = {
-    backgroundColor: `#9e9e9e`,
-    backgroundRepeat: `no-repeat`,
-    backgroundPosition: `top`,
-    backgroundSize: `cover`,
-    backgroundImage: `url(${imageTest})`,
-  };
-
-  const ChannelImg = {
-    backgroundColor: `gold`,
-    backgroundRepeat: `no-repeat`,
-    backgroundPosition: `top`,
-    backgroundSize: `150%`,
-    backgroundImage: `url(${imageTest})`,
-  };
-
-  // ? 변수로 받아오는 값은 css가 안걸림... 왜죠...?
-  // ? line97은 왜 안걸릴까..?
-  const Overflow = {
-    overflow: `hidden`,
-    textOverflow: `ellipsis`,
-    // lineClamp: `2`,
-    // ? CSSProperties 형식에서 에러뜸
-    // ! webkt 프레임워크 사용해서 해결 가능 -> 사용할 것인가?
-  };
-
   return (
-    <div className="video">
-      <figure className="thumnail" style={ThunmbnailImg}>
-        <div className="hidden">
-          <div className="lastview">a</div>
-          <div className="addlist">b</div>
+    <div className="video-box">
+      <a href={video}>
+        <div className="thumbnail">
+          <img alt={title} src={thumb_nail} />
+          <div className="runtime">{run_time}</div>
         </div>
-        <div className="runtime">
-          <div className="runtime-item">
-            {(runtime - (runtime % 60)) / 60}:{runtime % 60}
+        <div className="detail">
+          <div className="channel-icon"></div>
+          <div className="meta">
+            <div className="title">{title}</div>
+            <div className="channel-name">{uploader}</div>
+            <div className="etc">
+              <div className="views">{watch_count}</div>
+              <div className="created">{created_at}</div>
+            </div>
           </div>
+          <div className="menu"></div>
         </div>
-      </figure>
-      <div className="videoDesc">
-        <div className="videoImg" style={ChannelImg}>
-          {/* <img src={ChannelImg.backgroundImage} /> */}
-        </div>
-        <div className="videoContent">
-          <div className="videoName">
-            <p className="videoName-item" style={Overflow}>
-              {thumbnail}
-            </p>
-          </div>
-          <div className="videoChannel">
-            <p className="videoChannel-item">{channel}</p>
-          </div>
-          <CreateAt views={views} create_at={create_at} />
-        </div>
-        <div className="hiddenAdd">...</div>
-      </div>
+      </a>
     </div>
   );
 };
@@ -154,7 +104,7 @@ const VideoContainer = (props: VideoContainerProps) => {
       //   setLoadVideo(false);
       // }, 1000);
 
-      fetch("http://www.neotubei.kro.kr/neotubei/v1/browse/")
+      fetch("http://www.neotubei.kro.kr/browse/")
         .then((res) => res.json())
         .then((json) => {
           setVideoList((vl) => vl.concat(json.video));
