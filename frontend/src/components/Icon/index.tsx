154dc478dbc7b4c1222f83e417187b164d511a31
Jaehun Kim <wogns3623@gmail.com>
2021-02-24 20:06:22 +0900

    재훈이꺼랑 내꺼 합쳐진거(오류)

--- frontend/src/components/Icon/index.tsx
+++ frontend/src/components/Icon/index.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import ButtonIcon from "./ButtonIcon";
 import DescIcon from "./DescIcon";
-import { BasicProps } from "types";
-import { SvgEnum, svgList } from "assets/svg";
+import { SingleWrapperProps } from "types";
 
 import "./Icon.scss";
 
-export type IconProps = BasicProps & {
-  children?: React.ReactNode;
-  type?: SvgEnum;
-};
-
-const Icon = ({ className, children, type }: IconProps) => {
-  if (type && !className) className = type;
-  if (type) {
-    children = svgList[type];
-  }
-
+const Icon = ({ children, className }: SingleWrapperProps) => {
   return (
     <div className={`react-icon ${className ? className : ""}`}>{children}</div>
   );
